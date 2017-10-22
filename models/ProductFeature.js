7b086617f2c270e875ef797198e2feb8214812df
Igor Pavlenko <ipavlenko@inbox.ru>
2017-10-22 20:20:24 +0500

    Product feature

---
+++ models/ProductFeature.js
@@ -0,0 +1,18 @@
+const keystone = require('keystone')
+const Types = keystone.Field.Types
+
+const ProductDownload = new keystone.List('ProductFeature', {
+  map: { name: 'name' },
+  autokey: { path: 'slug', from: 'name', unique: true }
+})
+
+ProductDownload.add({
+  name: { type: String, required: true },
+  title: { type: String },
+  image: { type: Types.CloudinaryImage },
+  image2x: { type: Types.CloudinaryImage }
+})
+
+ProductDownload.relationship({ ref: 'Product', path: 'product', refPath: 'downloads' })
+
+ProductDownload.register()
