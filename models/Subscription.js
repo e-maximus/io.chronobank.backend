808fc5d601df14f2f7f6e69acfc0d3975064e078
Igor Pavlenko <ipavlenko@inbox.ru>
2017-11-01 00:13:21 +0500

    Error handler

--- models/Subscription.js
+++ models/Subscription.js
@@ -2,6 +2,7 @@ const keystone = require('keystone')
 const Types = keystone.Field.Types
 
 const Subscription = new keystone.List('Subscription', {
+  map: { name: 'email' },
   nocreate: true,
   noedit: true,
 })
