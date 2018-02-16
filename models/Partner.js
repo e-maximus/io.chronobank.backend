51775352cba3d1ba8a6575a2cfb7604bba7de3f4
Sozinov Alexander <sanzeting@mail.ru>
2018-02-16 18:05:05 +0300

    CMS-46: Testimonials, Partners, Members, Jobs (models) was updated

--- models/Partner.js
+++ models/Partner.js
@@ -2,6 +2,7 @@ const keystone = require('keystone')
 const config = require('config')
 const download = require('../utils').download.intoDirectory(config.get('uploads.dir'))
 const Types = keystone.Field.Types
+const { withTranslation, applyTranslationHook } = require('../utils')
 
 const Partner = new keystone.List('Partner', {
   map: { name: 'title' },
@@ -15,9 +16,16 @@ Partner.add({
   icon: { type: Types.CloudinaryImage },
   icon2x: { type: Types.CloudinaryImage },
   brief: { type: Types.Html, wysiwyg: true, height: 150 }
-})
+},
+  'Internationalization',
+  withTranslation.all({
+    brief: { type: Types.Html, wysiwyg: true, label: 'Brief', height: 150 }
+  })
+)
 
-Partner.defaultColumns = 'title, icon, url'
+applyTranslationHook(Partner.schema)
+
+Partner.defaultColumns = 'title, icon, url, i18nTranslations'
 
 Partner.schema.post('save', async (d) => {
   await Promise.all([d.icon, d.icon2x]
